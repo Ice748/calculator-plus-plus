978a33ade0b084fa35a00842a067aee80fbf52ca
Ice748 <dvinogradov174@gmail.com>
2026-06-18 10:23:54 +0300

    Update main.rs
    add comments and remove module structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,60 @@
-mod scenes;
+use std::io::{self, Write};
+use std::process::Command;
 
+// Main scene
 fn main() {
-    crate::scenes::menu();
+    const BTNS: &[&str] = &["Standard mode", "About", "Exit"];
+    let mut btn: usize = 0;
+    let mut data = String::new();
+
+    loop {
+        data.clear();
+
+        clear();
+        println!("Calculator++\n");
+
+        for i in 0..BTNS.len() {
+            let prefix = if i == btn { ">>" } else { ">" };
+            println!("{} {}", prefix, BTNS[i]);
+        }
+
+        io::stdin().read_line(&mut data).expect("Error to read line");
+        if data.trim().to_lowercase() == "e" {
+            match btn {
+                0 => standard_mode(),
+                1 => about(),
+                2 => {
+                    clear();
+                    std::process::exit(0);
+                }
+                _ => {}
+            }
+        } else {
+            btn = (btn + 1) % BTNS.len();
+        }
+    }
+}
+
+// Other scenes
+fn standard_mode() {
+    let mut data = String::new();
+
+    clear();
+    io::stdin().read_line(&mut data).expect("Error to read line");
+}
+
+fn about() {
+    let mut data = String::new();
+
+    clear();
+    io::stdin().read_line(&mut data).expect("Error to read line");
+}
+
+// Utilities
+fn clear() {
+    if cfg!(target_os = "windows") {
+        let _ = Command::new("cmd").args(["/C", "cls"]).status();
+    } else {
+        let _ = Command::new("clear").status();
+    }
 }
